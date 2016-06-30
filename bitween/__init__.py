b55a1ac0b6434866e33864f1476a87c467cff31d
jan <jan@kwoh.de>
2016-07-01 01:30:01 +0200

    basic config to fix doc build

--- bitween/__init__.py
+++ bitween/__init__.py
@@ -7,9 +7,20 @@ import os
 
 here = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 
-with open(os.path.join(here, '..', 'conf.json')) as f:
-    conf = json.load(f)
-
+if os.path.isfile(os.path.join(here, '..', 'conf.json')):
+    with open(os.path.join(here, '..', 'conf.json')) as f:
+        conf = json.load(f)
+else:
+    conf = {
+        "xmpp_accounts": [],
+        "save_path": "share",
+        "enable_web_api": True,
+        "enable_ipv4": True,
+        "enable_ipv6": True,
+        "enable_upnp": True,
+        "enable_natpmp": True
+    }
+    
 # create default dir for storing data we leech
 save_path = conf.get('save_path', 'share')
 if not os.path.isdir(save_path):
