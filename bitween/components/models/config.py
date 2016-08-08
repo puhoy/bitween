df868d614a657ea28d4f2a1f8aa6931aed759e80
jan <jan@kwoh.de>
2016-08-08 17:11:02 +0200

    config places

--- bitween/components/models/config.py
+++ bitween/components/models/config.py
@@ -2,23 +2,27 @@ import os
 import json
 from . import logger
 
+from os.path import expanduser
+home = expanduser("~")
+
 
 here = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 
 if os.path.isfile(os.path.join(here, '..', '..', 'conf.json')):
     with open(os.path.join(here, '..', '..', 'conf.json')) as f:
         conf = json.load(f)
+
+elif os.path.isfile(os.path.join(home, '.bitween.json')):
+    with open(os.path.join(home, '.bitween.json')) as f:
+        conf = json.load(f)
+
 else:
     logger.error('could not find conf.json')
-    conf = {
-        "xmpp_account": {},
-        "save_path": "share",
-        "enable_web_api": True,
-        "enable_ipv4": True,
-        "enable_ipv6": True,
-        "enable_upnp": True,
-        "enable_natpmp": True
-    }
+    print('no config file found!')
+    print('you can find a sample config file in %s. (fill out and put it in ~/.bitween.json)' % os.path.abspath(os.path.join(here, '..', '..', 'conf.json.dist')))
+    exit(0)
+
+
 
 # create default dir for storing data we leech
 save_path = conf.get('save_path', 'share')
